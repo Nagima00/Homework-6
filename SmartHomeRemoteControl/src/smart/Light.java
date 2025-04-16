738e730a284c947e2265702f68858e9106cfd2ce
Nagima00 <nagima.tasbalta@narxoz.kz>
2025-04-16 15:27:58 +0500

    Light.java

--- SmartHomeRemoteControl/src/smart/Light.java
+++ SmartHomeRemoteControl/src/smart/Light.java
@@ -1,1 +1,9 @@
-// Light.java
+public class Light {
+    public void turnOn() {
+        System.out.println("[Light] Turning ON");
+    }
+
+    public void turnOff() {
+        System.out.println("[Light] Turning OFF");
+    }
+}
