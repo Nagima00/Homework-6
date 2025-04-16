a422e930dd5d2159bfb1bfea006ff9b47c3caa62
Nagima00 <nagima.tasbalta@narxoz.kz>
2025-04-16 15:28:32 +0500

    Thermostat.java

--- SmartHomeRemoteControl/src/smart/Thermostat.java
+++ SmartHomeRemoteControl/src/smart/Thermostat.java
@@ -1,1 +1,13 @@
-// Thermostat.java
+public class Thermostat {
+    private int temperature;
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+        System.out.println("[Thermostat] Setting temperature to " + temperature + "°C");
+    }
+
+    public void undoTemperature(int previousTemperature) {
+        this.temperature = previousTemperature;
+        System.out.println("[Thermostat] Reverting to previous temperature: " + previousTemperature + "°C");
+    }
+}
