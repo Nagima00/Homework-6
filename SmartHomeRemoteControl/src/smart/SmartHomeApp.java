9c0efe84112b31145506894b27f5b6aeee75ae77
Nagima00 <nagima.tasbalta@narxoz.kz>
2025-04-16 15:31:11 +0500

    SmartHomeApp.java

--- SmartHomeRemoteControl/src/smart/SmartHomeApp.java
+++ SmartHomeRemoteControl/src/smart/SmartHomeApp.java
@@ -1,1 +1,18 @@
-// SmartHomeApp.java
+public class SmartHomeApp {
+    public static void main(String[] args) {
+        Light light = new Light();
+        Thermostat thermostat = new Thermostat();
+
+        Command turnOnLight = new TurnOnLightCommand(light);
+        Command setThermostat = new SetThermostatCommand(thermostat, 22);
+
+        SmartHomeRemoteControl remote = new SmartHomeRemoteControl();
+        remote.addCommand("light_on", turnOnLight);
+        remote.addCommand("set_thermo", setThermostat);
+
+        remote.pressButton("light_on");
+        remote.pressButton("set_thermo");
+
+        remote.undoButton("set_thermo");
+    }
+}
