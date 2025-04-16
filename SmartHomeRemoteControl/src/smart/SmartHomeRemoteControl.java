a135a90eb3e3792d2c119a5c143645b750d6bef4
Nagima00 <nagima.tasbalta@narxoz.kz>
2025-04-16 16:18:44 +0500

    SmartHomeRemoteControl.java

--- SmartHomeRemoteControl/src/smart/SmartHomeRemoteControl.java
+++ SmartHomeRemoteControl/src/smart/SmartHomeRemoteControl.java
@@ -1,10 +1,8 @@
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Stack;
 
 public class SmartHomeRemoteControl {
     private Map<String, Command> commandSlots = new HashMap<>();
-    private Stack<Command> history = new Stack<>();
 
     public void addCommand(String slot, Command command) {
         commandSlots.put(slot, command);
@@ -14,18 +12,13 @@ public class SmartHomeRemoteControl {
         Command command = commandSlots.get(slot);
         if (command != null) {
             command.execute();
-            history.push(command);
-        } else {
-            System.out.println("No command assigned to slot: " + slot);
         }
     }
 
-    public void undoButton() {
-        if (!history.isEmpty()) {
-            Command lastCommand = history.pop();
-            lastCommand.undo();
-        } else {
-            System.out.println("No command to undo.");
+    public void undoButton(String slot) {
+        Command command = commandSlots.get(slot);
+        if (command != null) {
+            command.undo();
         }
     }
 }
