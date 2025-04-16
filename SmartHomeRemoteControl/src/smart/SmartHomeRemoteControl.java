c071c972f7438959d51404b5cc113942ee7d3df6
Nagima00 <nagima.tasbalta@narxoz.kz>
2025-04-16 15:30:22 +0500

    SmartHomeRemoteControl.java

--- SmartHomeRemoteControl/src/smart/SmartHomeRemoteControl.java
+++ SmartHomeRemoteControl/src/smart/SmartHomeRemoteControl.java
@@ -1,1 +1,24 @@
-// SmartHomeRemoteControl.java
+import java.util.HashMap;
+import java.util.Map;
+
+public class SmartHomeRemoteControl {
+    private Map<String, Command> commandSlots = new HashMap<>();
+
+    public void addCommand(String slot, Command command) {
+        commandSlots.put(slot, command);
+    }
+
+    public void pressButton(String slot) {
+        Command command = commandSlots.get(slot);
+        if (command != null) {
+            command.execute();
+        }
+    }
+
+    public void undoButton(String slot) {
+        Command command = commandSlots.get(slot);
+        if (command != null) {
+            command.undo();
+        }
+    }
+}
