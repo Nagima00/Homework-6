2771424938b88b8a1fae11073cd8fe14544c277c
Nagima00 <nagima.tasbalta@narxoz.kz>
2025-04-16 15:26:04 +0500

    HandlerFactory.java

--- TechSupportHandler/src/handlers/HandlerFactory.java
+++ TechSupportHandler/src/handlers/HandlerFactory.java
@@ -1,1 +1,10 @@
-// HandlerFactory.java
+public class HandlerFactory {
+    public static SupportHandler createHandlerChain() {
+        SupportHandler faq = new FAQBotHandler();
+        SupportHandler junior = new JuniorSupportHandler();
+        SupportHandler senior = new SeniorSupportHandler();
+
+        faq.setNext(junior).setNext(senior);
+        return faq;
+    }
+}
