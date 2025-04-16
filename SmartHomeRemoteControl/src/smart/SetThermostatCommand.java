727312e562edfdaba9c2f2184fc528048e470b67
Nagima00 <nagima.tasbalta@narxoz.kz>
2025-04-16 15:29:44 +0500

    SetThermostatCommand.java

--- SmartHomeRemoteControl/src/smart/SetThermostatCommand.java
+++ SmartHomeRemoteControl/src/smart/SetThermostatCommand.java
@@ -1,1 +1,21 @@
-// SetThermostatCommand.java
+public class SetThermostatCommand implements Command {
+    private Thermostat thermostat;
+    private int previousTemperature;
+    private int newTemperature;
+
+    public SetThermostatCommand(Thermostat thermostat, int newTemperature) {
+        this.thermostat = thermostat;
+        this.newTemperature = newTemperature;
+    }
+
+    @Override
+    public void execute() {
+        previousTemperature = thermostat.getTemperature();
+        thermostat.setTemperature(newTemperature);
+    }
+
+    @Override
+    public void undo() {
+        thermostat.undoTemperature(previousTemperature);
+    }
+}
