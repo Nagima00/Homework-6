8d9ef28ec707daf384f8572bc845a90942395b30
Nagima00 <nagima.tasbalta@narxoz.kz>
2025-04-16 15:26:39 +0500

    TechSupportApp.java

--- TechSupportHandler/src/handlers/TechSupportApp.java
+++ TechSupportHandler/src/handlers/TechSupportApp.java
@@ -1,1 +1,10 @@
-// TechSupportApp.java
+public class TechSupportApp {
+    public static void main(String[] args) {
+        SupportHandler faq = HandlerFactory.createHandlerChain();
+
+        String[] issues = {"password_reset", "refund_request", "account_ban", "unknown_bug"};
+        for (String issue : issues) {
+            faq.handle(issue);
+        }
+    }
+}
