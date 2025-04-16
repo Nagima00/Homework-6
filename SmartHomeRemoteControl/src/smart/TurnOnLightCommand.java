a2b00fe35f350794316d117a730e48ebc028fa8d
Nagima00 <nagima.tasbalta@narxoz.kz>
2025-04-16 15:29:02 +0500

    TurnOnLightCommand.java

--- SmartHomeRemoteControl/src/smart/TurnOnLightCommand.java
+++ SmartHomeRemoteControl/src/smart/TurnOnLightCommand.java
@@ -1,1 +1,17 @@
-// TurnOnLightCommand.java
+public class TurnOnLightCommand implements Command {
+    private Light light;
+
+    public TurnOnLightCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOn();
+    }
+
+    @Override
+    public void undo() {
+        light.turnOff();
+    }
+}
